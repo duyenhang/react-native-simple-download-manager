891c912e6c9229dcd84570369f35524141a9a87e
Hang Kim Duyen <duyenhang@poetadigital.com>
2019-02-28 17:37:23 +0700

    change file name

--- android/src/main/java/com/masteratul/downloadmanager/Downloader.java
+++ android/src/main/java/com/masteratul/downloadmanager/Downloader.java
@@ -49,7 +49,7 @@ public class Downloader {
         
 
         if(external){
-            request.setDestinationInExternalPublicDir(external_path, saveAsName);
+            request.setDestinationInExternalPublicDir(external_path, downloadUri.lastPathSegment);
 
         }else{
             request.setDestinationInExternalFilesDir(context, Environment.DIRECTORY_DOWNLOADS, saveAsName);
